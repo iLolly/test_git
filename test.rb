2d144294bfd55ede59018ab183bd5a4041d37763
Lolly <cclolly@gmail.com>
2015-06-14 05:37:04 +0800

    update file info

--- test.rb
+++ test.rb
@@ -1,3 +1,17 @@
+#!/usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+
+#=============================================================================
+#     FileName: test.rb
+#         Desc:
+#       Author: Lolly
+#        Email: cclolly@gmail.com
+#     HomePage:
+#      Version: 0.0.1
+#   LastChange: 2015-06-14 05:35:52
+#      History:
+#=============================================================================
+
 class Demo
 
   def initialize(string = 'hello')
@@ -12,3 +26,4 @@ end
 
 demo = Demo.new('test')
 p demo.say
+
